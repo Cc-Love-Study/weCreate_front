d454ae75efa5279b52e43b80340fbe904fd8f968
钰 <2314709037@qq.com>
2024-02-01 01:04:01 +0800

    完成登录页面搭建

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ import (
 func main() {
 	fmt.Println("cc")
 	r := gin.Default()
+	r.LoadHTMLGlob("views/*")
+	r.Static("../static", "static")
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"msg": "begin  weCreate project front"})
+	})
+	r.GET("/login", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "login.html", gin.H{})
 	})
 	r.Run(":8080")
 }
