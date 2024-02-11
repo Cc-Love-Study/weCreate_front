00280694fc63ef437761be6a1d58487154deffc2
钰 <2314709037@qq.com>
2024-02-11 23:55:42 +0800

    完成一些页面基本框架 完成一些路由

--- controllers/indexController.go
+++ controllers/indexController.go
@@ -19,5 +19,16 @@ func NewIndexController(r *gin.Engine, name string, indexService *services.Index
 
 // 路由注册
 func (i *IndexController) InitIndexController() {
+	// 主页
 	i.IndexRouterGroup.GET("/index", i.IndexService.GetIndexPage)
+	// 社区
+	i.IndexRouterGroup.GET("/community", i.IndexService.GetCommunityPage)
+	// 公告
+	i.IndexRouterGroup.GET("/announcement", i.IndexService.GetAnnouncementPage)
+	// 发布
+	i.IndexRouterGroup.GET("/posting", i.IndexService.GetPostingPage)
+
+	i.IndexRouterGroup.GET("/posting/postTopic", i.IndexService.GetPostingTopicPage)
+	// 关注
+	i.IndexRouterGroup.GET("/concern", i.IndexService.GetConcernPage)
 }
--- services/indexServer.go
+++ services/indexServer.go
@@ -19,3 +19,28 @@ func (u *IndexService) GetIndexPage(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "index.html", gin.H{})
 	return
 }
+
+func (u *IndexService) GetCommunityPage(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "community.html", gin.H{})
+	return
+}
+
+func (u *IndexService) GetAnnouncementPage(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "announcement.html", gin.H{})
+	return
+}
+
+func (u *IndexService) GetPostingPage(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "posting.html", gin.H{})
+	return
+}
+
+func (u *IndexService) GetPostingTopicPage(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "postwrite.html", gin.H{})
+	return
+}
+
+func (u *IndexService) GetConcernPage(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "concern.html", gin.H{})
+	return
+}
